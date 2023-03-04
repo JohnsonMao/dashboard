adea32f5f50c82322b05dc7ab637f586a958b60c
Johnson Mao <tutelary.maomao@gmail.com>
2023-03-04 18:05:01 +0800

    add: yup & formik

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,14 @@
+import Box from '@mui/material/Box';
+import Input from './Input';
+
 function Form() {
-    return <div>Form</div>;
+    return (
+        <Box component="form">
+            <Input label="帳號" />
+            <Input label="密碼" type="password" />
+            <Input label="數字" type="number" />
+        </Box>
+    );
 }
 
 export default Form;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,7 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
-import TextField from '@/components/Form/TextField';
+import Form from '@/components/Form/Form';
 
 function Login() {
     return (
@@ -11,11 +11,7 @@ function Login() {
                 <Typography>登入</Typography>
             </Box>
             <Box sx={{ px: 2, pb: 1 }}>
-                <TextField label="帳號" />
-                <Box />
-                <TextField label="密碼" type="password" />
-                <Box />
-                <TextField label="數字" type="number" />
+                <Form />
             </Box>
         </Paper>
     );
