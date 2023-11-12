5cec87b84d2b9d85e7cc452f6a59460444886824
Johnson Mao <tutelary.maomao@gmail.com>
2023-11-12 18:51:45 +0800

    🧱 add tailwind CSS and text

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,251 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header>
+        <a href="#">Miao</a>
+        <nav>
+          <ul>
+            <li>候選人主張</li>
+            <li>最新活動</li>
+            <li>政策議題</li>
+            <li>小額捐款</li>
+            <li>服務信箱</li>
+            <li>FB</li>
+            <li>IG</li>
+            <li>YT</li>
+          </ul>
+        </nav>
+      </header>
+      <section>
+        <h1>喵的未來 人的希望</h1>
+        <h2>喵立翰 Miao Li-Han</h2>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          我堅信，藉由推動更完善的貓咪福利和相關政策，更是間接地投資於台灣的未來。
+          畢竟，民眾的身心健康與工作熱情是推動經濟的核心動力。
+          透過完善的貓咪福利政策，為台灣的 GDP 經濟帶來巨大效益。
+          因此，我期望能在立法院內推進這些政策，確保每一隻貓咪都能得到他們應有的照顧，
+          同時也為台灣的經濟發展助一臂之力。
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h3>讓我們一同護航台灣的幸福經濟，從照顧每一隻貓咪開始！</h3>
+      </section>
+      <main>
+        <section>
+          <h2>掌握喵喵最新活動！</h2>
+          <ul>
+            <li>
+              <h3>
+                <span>12/26</span>
+                參與台北寵物論壇，爭取貓咪友善環境
+              </h3>
+              <p>
+                炎炎夏日的周六，我走進了台北寵物論壇，帶著一副貓耳髮箍，
+                決定要全力宣傳「貓咪至上」的理念！
+                我相信，我們的都市中，每一隻貓咪都應該有自己的 VIP 休憩空間。
+              </p>
+            </li>
+            <li>
+              <h3>
+                <span>12/24</span>
+                掃街模式開啟！帶著你的貓耳，來和我一起走！
+              </h3>
+              <p>
+                街上氣氛真的很棒，從小孩到大人，甚至有些狗狗朋友都帶著貓耳來找我握手，真的太可愛了！
+                這次的活動不僅讓我看到大家的熱情，更加堅定了我推進「貓咪友善環境」政策的決心。
+              </p>
+            </li>
+            <li>
+              <h3>
+                <span>12/20</span>
+                收容所模特兒大比拼！
+              </h3>
+              <p>
+                今天的收容所不再是一片寂靜。
+                為了讓更多人認識到這裡的毛孩子，我們舉辦了一場前所未有的「模特兒走秀」！
+              </p>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h2 className="hidden">政策議題</h2>
+          <ul>
+            <li>
+              <h3>
+                <span>政策 No.1</span>
+                為毛孩子謀福利！推動寵物醫療保障方案
+              </h3>
+              <ul>
+                <li>
+                  <h4>01 設立寵物醫療基金</h4>
+                  <p>
+                    每年撥款新台幣 10 億元，專款專用於支援家庭寵物的醫療費用
+                  </p>
+                </li>
+                <li>
+                  <h4>02 提供醫療補助</h4>
+                  <p>
+                    每隻寵物每年可獲得高達新台幣 20,000
+                    元的醫療補助，減輕飼主的經濟壓力
+                  </p>
+                </li>
+                <li>
+                  <h4>03 合作動物醫院</h4>
+                  <p>目前已有和超過 200 家動物醫院進行初步的合作討論</p>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <h3>
+                <span>政策 No.2</span>
+                打造休閒天堂！推廣寵物休閒與娛樂場所
+              </h3>
+              <ul>
+                <li>
+                  <h4>01 建立寵物公園</h4>
+                  <p>
+                    每年撥款新台幣 5 億元，用於在各大都市建立專屬的寵物公園。
+                    根據初步規劃，預計在第一年內，將在全國範圍內建立至少 10
+                    座寵物公園。
+                  </p>
+                </li>
+                <li>
+                  <h4>02 推廣寵物友善商家</h4>
+                  <p>
+                    鼓勵商家提供寵物友善的環境，並為參與的商家提供稅收優惠。
+                    預計在政策實施後的首年，將有超過 500 家商家加入此計畫。
+                  </p>
+                </li>
+                <li>
+                  <h4>03 舉辦寵物活動和工作坊</h4>
+                  <p>
+                    與各大寵物社團和組織合作，每年舉辦至少 5 場大型的寵物活動，
+                    包括寵物才藝比賽、飼養知識工作坊等。
+                  </p>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <h3>
+                <span>政策 No.3</span>
+                推廣寵物飼養教育，讓愛更加專業！
+              </h3>
+              <ul>
+                <li>
+                  <h4>01 建立寵物飼養教育中心</h4>
+                  <p>
+                    每年撥款新台幣 3
+                    億元，用於在全國各地建立專業的寵物飼養教育中心。
+                    預計在第一年內，在全國至少 5 大城市設立教育中心。
+                  </p>
+                </li>
+                <li>
+                  <h4>02 推廣寵物飼養課程</h4>
+                  <p>
+                    與學校、社區組織和寵物社團合作，推出一系列免費的寵物飼養課程。
+                    預計每年將有超過 10,000 名市民受益。
+                  </p>
+                </li>
+                <li>
+                  <h4>03 製作教育資料</h4>
+                  <p>
+                    出版寵物飼養手冊、影片和線上課程，
+                    讓所有有意飼養寵物的家庭都能輕鬆取得正確的知識。
+                  </p>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </section>
+      </main>
+      <section>
+        <h3>您的小筆捐款，是每隻毛孩未來的大大動力！</h3>
+        <p>目前贊助總金額：NT$ 987,655,873</p>
+        <ul>
+          <li>
+            <div>
+              <div>「 喵星人之友 」</div>
+              <div>捐款新台幣600元</div>
+              <button>馬上支持 !</button>
+              <div>已有 9,957 人贊助</div>
+            </div>
+          </li>
+          <li>
+            <div>
+              <div>「 喵星人之友 」</div>
+              <div>捐款新台幣600元</div>
+              <button>馬上支持 !</button>
+              <div>已有 9,957 人贊助</div>
+            </div>
+          </li>
+          <li>
+            <div>
+              <div>「 喵星大使 」</div>
+              <div>捐款新台幣6,000元</div>
+              <button>馬上支持 !</button>
+              <div>已有 2,000 人贊助</div>
+            </div>
+          </li>
+          <li>
+            <div>
+              <div>「 喵星傳奇 」</div>
+              <div>捐款新台幣60,000元</div>
+              <button>馬上支持 !</button>
+              <div>已有 999 人贊助</div>
+            </div>
+          </li>
+        </ul>
+        <div>
+          <div>「 自訂贊助金額 」</div>
+          <div>目前小額贊助總金額：NT$ 655,873</div>
+          <label htmlFor="money">NT$</label>
+          <input type="text" id="money" />
+          <button>馬上支持 !</button>
+        </div>
+      </section>
+      <section>
+        <h3>您的聲音，我們的行動！</h3>
+        <p>
+          親愛的鄉親，每一位市民的意見都是我們社區前進的原動力。無論大小事，我都誠摯希望聽到您的建議。
+          分享您的想法，一同為我們的未來打造更美好！
+        </p>
+        <form>
+          <label htmlFor="name">您的姓名</label>
+          <input type="text" id="name" />
+          <label htmlFor="email">您的Email</label>
+          <input type="email" id="email" />
+          <label htmlFor="phone">您的手機</label>
+          <input type="tel" id="phone" />
+          <label htmlFor="note">您的建言</label>
+          <textarea id="note" placeholder="我想和喵喵說..." />
+          <button>送出</button>
+          <p>為了喵喵站出來！</p>
+        </form>
+      </section>
+      <footer>
+        <nav>
+          <div>快速連結</div>
+          <ul>
+            <li>首頁</li>
+            <li>最新活動</li>
+            <li>政策議題</li>
+            <li>小額捐款</li>
+          </ul>
+        </nav>
+        <div>
+          <div>聯絡資訊</div>
+          <div>辦公室地址：喵星區，毛茸茸大道88號，喵喵大樓3樓</div>
+          <div>電話：(02) 888-5678</div>
+          <div>電子郵件地址：meowoffice@linmeow.tw</div>
+        </div>
+        <div>
+          <div>關注喵喵最新動態</div>
+          <ul>
+            <li>YT</li>
+            <li>FB</li>
+            <li>IG</li>
+          </ul>
+          <div>版權聲明：© 2023 喵立翰 Miao Li-Han 版權所有。</div>
+        </div>
+      </footer>
     </>
   )
 }
