ef4ee96effd40d6229cb4a35e4530053ad882fab
Johnson Mao <tutelary.maomao@gmail.com>
2023-11-14 09:59:28 +0800

    🔥 remove repo

--- src/components/Icons/_icon.type.tsx
+++
@@ -1,4 +0,0 @@
-export type IconProps = {
-  rectFill?: '#00A199' | '#F4CA80' | 'none' | 'white',
-  pathFill?: '#00A199' | '#F4CA80' | 'none' | 'white'
-}
--- src/utils/cn.ts
+++
@@ -1,6 +0,0 @@
-export default function cn(...classValue: (string | false)[]) {
-  return classValue.reduce((result: string, item) => {
-    if (!item) return result;
-    return result + ' ' + item
-  }, '')
-}
